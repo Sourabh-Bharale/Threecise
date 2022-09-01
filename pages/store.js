7b842a8ca2ad2460a89251da3b9d87a91d174ae7
Aryan Jangid <aryanjangid2511@gmail.com>
2022-09-01 16:55:48 +0530

    landing page code refined

--- pages/store.js
+++ pages/store.js
@@ -2,3 +2,8 @@ export let welcome = true;
 export const changeWelcome = () => {
     welcome = false;
 }
+
+export const colors = ['#704F4F', '#FFCF23', '#64D6E2', '#EF9F64', '#9988CD', '#A500F2', '#EC7272', '#B9FFF8', '#F94892', '#A66CFF', '#FD5D5D', '#FF8FB1', '#0096FF'];
+
+export const dances = ["Dance", "Thriller Moves", "Squats", "Hip Hop", "Fun Moves", "Excercises", "Excercise", "Dance", "Thriller", "Squats", "Hip Hop", "Fun Moves", "Excercises"];
+export const pages = ['dance', 'thriller', 'squats', 'hiphop', 'funMoves', 'excercises', 'excercise', 'dance', 'thriller', 'squats', 'hiphop', 'funMoves', 'excercises']
